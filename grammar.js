71556f7e7d8f369da9cf499c453766ca8af4ddda
homedirectory <47361170+homedirectory@users.noreply.github.com>
2023-06-08 20:58:48 +0300

    make number prefix non-optional
    this makes the following parse as symbols instead of numbers:
    a
    'a
    ab
    f
    
    hexadecimal must not be specified without a prefix

--- grammar.js
+++ grammar.js
@@ -410,9 +410,9 @@ function number_base(n) {
       choice(
         seq(
           optional(exactness),
-          optional(prefix)),
+          prefix),
         seq(
-          optional(prefix),
+          prefix,
           optional(exactness))),
       number);
 
@@ -492,7 +492,7 @@ function extflonum(n) {
         sign,
         inexact_special));
 
-  return seq(optional(prefix), inexact_real);
+  return seq(prefix, inexact_real);
 }
 
 // number }}}
